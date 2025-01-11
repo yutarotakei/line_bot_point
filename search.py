4f98b55ca683e5fa36ba1125c86913f20e066827
YutaroTakei <yutarotakei@icloud.com>
2025-01-11 23:05:07 +0900

    微修正

--- search.py
+++ search.py
@@ -26,14 +26,12 @@ def search_campaign_by_name(keyword):
         formatted_results = []
         for point_name, campaigns in campaigns_by_point.items():
 
-            formatted_results.append("\n")  # 改行を1つ追加
-            formatted_results.append("\n")  # 改行を1つ追加
             formatted_results.append(f"- {point_name}-")
             formatted_results.extend(
-                [f"『{campaign[1]}』\n{campaign[2]}" for campaign in campaigns]
+                [f"\n\n『{campaign[1]}』\n{campaign[2]}" for campaign in campaigns]
             )
 
-        return f"おっ、『{keyword}』に関連するキャンペーンを探しておるのか？ふむふむ……おおっ、こんなのが見つかったぞい！これは見逃せんぞい！" + "\n".join(formatted_results)
+        return f"おっ、『{keyword}』に関連するキャンペーンを探しておるのか？\n\nふむふむ……おおっ、こんなのが見つかったぞい！これは見逃せんぞい！" + "\n".join(formatted_results)
     else:
         return f"""……ん？残念ながらその名前ではキャンペーン情報は見つからんのう。ワシはポイントサイトや色んな会社の公式情報を参考にしておる。\n\n融通が利かなくて申し訳ないのじゃが正式名称じゃないとワシは認識できないのじゃ。ひらカナを変えたり、他のやり方で試せば出せるかもしれん。\n
 ちなみに、自治体商品券を探すなら、末尾に『市・区・町・村』をつけてくれると助かるぞい。ワシも頑張って探してみるから、また声をかけてくれのう！"""
